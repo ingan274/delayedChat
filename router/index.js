4cc518520bad00f7c206e14ee24ef6adc9c48aaa
ingan <isabelngan27@gmail.com>
2021-07-25 19:36:08 -0400

    why is it blankkkk

--- router/index.js
+++ router/index.js
@@ -7,8 +7,8 @@ const mccCrewRoutes = require("./mcccrew");
 router.use("/api/mcccrew", mccCrewRoutes)
 
 // If no API routes are hit, send the React app
-router.use(function (req, res) {
+router.use(function(req, res) {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+  });
 
 module.exports = router;
