e0a1550ef9a4f14caeff00b74144ef1dfc012e09
ingan <isabelngan27@gmail.com>
2021-07-26 00:24:08 -0400

    adding

--- server.js
+++ server.js
@@ -65,7 +65,6 @@ if (process.env.NODE_ENV === 'production') {
     // app.use(express.static(path.join(__dirname, './client/build')))
     // app.get('/', (req, res) => {
     //     res.sendFile(path.join(__dirname, './client/build/index.html'))
-    })
 
 //     const root = require('path').join(__dirname, 'client', 'build')
 //     app.use(express.static(root));
